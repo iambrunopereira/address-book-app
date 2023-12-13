85514f81b2caa5059cfdcc5e7f8cd77e0fc87dc3
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-13 10:48:47 +0000

    build: initial project structure

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
+}
+
+module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main >
+      <div >
+        <h1>
+          Hello world
+        </h1>
+      </div>
+    </main>
+  )
+}
