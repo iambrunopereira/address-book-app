88bd8e463ad22e8acf6955d152066d3727993195
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-18 12:38:40 +0000

    feat: added public path map

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,8 @@
 export const enableDevTools = process.env.NEXT_PUBLIC_ENABLE_REDUX_DEV_TOOLS === 'true';
 
 export const config = {
-    appName: "My Address Book"
+    appName: "My Address Book",
+    serviceApiUrl: process.env.NEXT_PUBLIC_SERVICE_API_URL,
 }
+
+export const publicRoutes = ['/login', '/signup'];
