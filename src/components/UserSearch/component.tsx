89b99097cee5e1faf24b2f8ad50dc45134179cb7
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-19 01:15:32 +0000

    refactor: search component to handle with custom filters

--- src/components/UserSearch/component.tsx
+++ src/components/UserSearch/component.tsx
@@ -1,117 +1,159 @@
-import UserCard from "@/components/UserCard/component";
-import { useFetchUsersQuery } from "@/store/services/usersApi";
-import { useSelector } from "@/store/store";
+import UserCard from '@/components/UserCard/component';
+import { useFetchUsersQuery } from '@/store/services/usersApi';
 
-import { debounce } from "lodash";
-import { useCallback, useEffect, useState } from "react";
+import { debounce } from 'lodash';
+import { useCallback, useEffect, useState } from 'react';
 
-import Box from "../common/Box";
-import { Grid } from "../common/Grid";
-import GridItem from "../common/GridItem/component";
-import { InfiniteScroll } from "../common/InfiniteScroll";
-import { TextField } from "../common/TextField";
-import styles from "./component.module.scss";
+import { useRouter, useSearchParams } from 'next/navigation';
+import Box from '../common/Box';
+import { Button } from '../common/Button';
+import { Grid } from '../common/Grid';
+import GridItem from '../common/GridItem/component';
+import { InfiniteScroll } from '../common/InfiniteScroll';
+import { TextField } from '../common/TextField';
+import { Typography } from '../common/Typography';
+import styles from './component.module.scss';
 
-const UsersSearchComponent: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [searchTermDirty, setSearchTermDirty] = useState<boolean>(false);
-  const [listUsers, setListUsers] = useState<any[]>([]);
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState(searchTerm);
-  const [nextPage, setNextPage] = useState(1);
-  const { data, isLoading, isFetching } = useFetchUsersQuery(
-    { name: debouncedSearchTerm, filter: {nextPage} }
-  );
+interface UserSearchProps {
+    settings: any;
+    clearHandler: () => void;
+}
 
-    const { menuOpened } = useSelector((state) => state.settings);
+const UsersSearchComponent: React.FC<UserSearchProps> = ({ settings, clearHandler, nationalities, gender}) => {
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    /* const [nationalities, setNationalities] = useState<string[]>([]);
+    const [gender, setGender] = useState<string>('all'); */
+    const [searchTermDirty, setSearchTermDirty] = useState<boolean>(false);
+    const [listUsers, setListUsers] = useState<any[]>([]);
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState(searchTerm);
+
+    const [nextPage, setNextPage] = useState(1);
+    const router = useRouter();
+    const searchParams = useSearchParams()
+    const { menuOpened } = settings;
+
+    const { data, isLoading, isFetching, refetch } = useFetchUsersQuery({
+        name: debouncedSearchTerm,
+        filter: { nextPage, nationalities, gender }
+    }, {refetchOnMountOrArgChange: true});
+
     useEffect(() => {
-      if (data?.data ) {
-        setListUsers((prevRepos) => [...prevRepos, ...data.data]);
+      const queryValue = searchParams.get('q') as string;
+      if (queryValue) {
+        setSearchTerm(queryValue);
+        setDebouncedSearchTerm(queryValue);
       }
-      
+  }, [searchParams]);
+    useEffect(() => {
+
+        if (data?.data) {
+            setListUsers(prevRepos => [...prevRepos, ...data.data]);
+        }
     }, [data]);
-
-  const debounceSearchTermUpdate = useCallback(
-    
-    debounce((searchValue) => {
+    const clearFilterAndRefetchData = () => {
       setListUsers([]);
-      setDebouncedSearchTerm(searchValue);
-      
-      setNextPage(1);
-    }, 500),
-    []
-  );
-
-  useEffect(() => {
-    if(searchTermDirty) {
-      debounceSearchTermUpdate(searchTerm);
-      return () => {
-        debounceSearchTermUpdate.cancel();
-      };
+      clearHandler();
     }
-  }, [searchTerm, debounceSearchTermUpdate, searchTermDirty]);
+    const debounceSearchTermUpdate = useCallback(
+        debounce(searchValue => {
+            setListUsers([]);
+            setDebouncedSearchTerm(searchValue);
+            let urlParams = searchValue !== '' ? `/?q=${encodeURIComponent(searchValue)}` : `/`
+            router.push(urlParams);
+            setNextPage(1);
+        }, 500),
+        []
+    );
 
-  const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> =
-    useCallback((event) => {
-      setSearchTerm(event.target.value);
-      !searchTermDirty && setSearchTermDirty(true);
-    }, [searchTermDirty]);
+    useEffect(() => {
+        if (searchTermDirty) {
+            debounceSearchTermUpdate(searchTerm);
+            return () => {
+                debounceSearchTermUpdate.cancel();
+            };
+        }
+    }, [searchTerm, debounceSearchTermUpdate, searchTermDirty]);
 
-  const loadMoreItems = () => {
-    if (!isLoading && !isFetching && data?.moreData) {
-        setNextPage(data?.nextPage);
-    }
-  };
+    const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
+        event => {
+            setSearchTerm(event.target.value);
+            !searchTermDirty && setSearchTermDirty(true);
+        },
+        [searchTermDirty]
+    );
 
-  return (
-    <>
-      <Box
-        className={`${styles.userSearchWrapper} ${menuOpened ? styles.menuOpened : '' }`}
-        
-      >
-        <TextField
-          label={"Search Users"}
-          placeholder="Search Users"
-          name="searchTerm"
-          value={searchTerm}
-          onChange={(e) => {handleSearchChange(e)}}
-        />
-      </Box>
-      <Box flex justifyContent="center" style={{ marginTop: "6rem" }}>
-        <InfiniteScroll
-          hasMore={data?.moreData}
-          loadMore={loadMoreItems}
-          isLoading={isLoading || isFetching}
-        >
-          <Grid
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            gap="10px"
-            flexWrap="wrap"
-            style={{
-              maxWidth: "88rem",
-            }}
-          >
+    const loadMoreItems = () => {
+        if (!isLoading && !isFetching && data?.moreData) {
+            setNextPage(data?.nextPage);
+        }
+    };
 
-            {listUsers.map((user, idx) => {
-              return (
-                <GridItem key={`user-card-${idx}`}flexGrow={1}>
-                  <UserCard user={user} />
-                </GridItem>
-              );
-            })}
-            
-          </Grid>
-          {listUsers.length === 0 && !data?.moreData && (
-                <p>No data available...</p>
-            )}
-            {listUsers.length !== 0 && !data?.moreData && (
-                <p>you reach the end of the data...</p>
-            )}
-        </InfiniteScroll>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Box className={`${styles.userSearchWrapper} ${menuOpened ? styles.menuOpened : ''}`}>
+                <TextField
+                    label={'Search Users'}
+                    placeholder="Search Users"
+                    name="searchTerm"
+                    value={searchTerm}
+                    onChange={e => {
+                        handleSearchChange(e);
+                    }}
+                />
+                <Box flex justifyContent="space-between">
+                    {(nationalities.length > 0 || gender !== 'all') ? (
+                        <>
+                            <Box flex>
+                              {nationalities.length > 0 && <Typography style={{marginRight: '1rem'}}><strong>Nationalities:</strong> {nationalities.toString()}</Typography>}
+                              {gender != 'all' && <Typography><strong>Gender:</strong> {gender}</Typography>}
+                            </Box>
+                            <Box>
+                                <Button variant="link" onClick={clearFilterAndRefetchData} style={{marginRight: '0.5rem'}}>
+                                    Clear
+                                </Button>
+                                <Button variant="link" onClick={() => router.push('/settings')}>
+                                    Manage
+                                </Button>
+                            </Box>
+                        </>
+                    ) : (
+                        <>
+                            <Typography>No filters selected...</Typography>
+                            <Box>
+                                <Button variant="link" onClick={() => router.push('/settings')}>
+                                    Add Filters
+                                </Button>
+                            </Box>
+                        </>
+                    )}
+                </Box>
+            </Box>
+            <Box flex justifyContent="center" style={{ marginTop: '8rem' }}>
+                <InfiniteScroll hasMore={data?.moreData} loadMore={loadMoreItems} isLoading={isLoading || isFetching}>
+                    <Grid
+                        flexDirection="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        gap="10px"
+                        flexWrap="wrap"
+                        style={{
+                            maxWidth: '88rem',
+                        }}
+                    >
+                        {listUsers.map((user, idx) => {
+                            return (
+                                <GridItem key={`user-card-${idx}`} flexGrow={1}>
+                                    <UserCard user={user} />
+                                </GridItem>
+                            );
+                        })}
+                    </Grid>
+                    {listUsers.length === 0 && !data?.moreData && <Typography style={{marginTop: "0.5rem"}}>No data available...</Typography>}
+                    {listUsers.length !== 0 && !data?.moreData && <Typography style={{marginTop: "0.5rem"}}>you reach the end of the data...</Typography>}
+                </InfiniteScroll>
+            </Box>
+        </>
+    );
 };
 
 export default UsersSearchComponent;
--- src/components/UserSearch/container.tsx
+++ src/components/UserSearch/container.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { setGender, setNationalities } from "@/store/slices/settingsSlice";
+import { useSelector } from "@/store/store";
+import { Gender } from "@/types/store";
+import { useDispatch } from "react-redux";
 import UsersSearchComponent from "./component";
 
 const UsersSearchContainer: React.FC = () => {
+  const settings = useSelector((state) => state.settings);
+  const dispatch = useDispatch();
+  
+  const clearFilters = () => {
+    dispatch(setNationalities([]));
+    dispatch(setGender(Gender.All));
+  }
 
   return (
-    <UsersSearchComponent />
+    <UsersSearchComponent settings={settings} clearHandler={clearFilters} gender={settings.gender} nationalities={settings.nationalities}/>
   );
 };
 
