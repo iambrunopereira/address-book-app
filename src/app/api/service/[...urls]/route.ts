a631ab2b36dbd7e0e5c01096d53e9293d93b6689
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-22 17:18:25 +0000

    fix: request route to handle will all the requests

--- src/app/api/service/[...urls]/route.ts
+++ src/app/api/service/[...urls]/route.ts
@@ -1,24 +1,34 @@
 import { config } from '@/configs';
+import { httpMethod } from '@/types';
 import { request } from '@/utils/requestHandler';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
-type Method = 'POST' | 'GET' | 'PUT' | 'DELETE' | 'PATCH';
 
-const requestHandler = async (req: NextRequest, method: Method, url: string, body: any, header: any) => {
+interface RequestBody {
+  [key: string]: unknown; 
+}
+
+interface RequestHeaders {
+  [key: string]: string;
+}
+interface ApiResponse<T> {
+  data: T;
+  status: number;
+}
+const requestHandler = async (req: NextRequest, method: httpMethod, url: string, body: RequestBody,
+  header: unknown) => {
 
     const cookieStore = cookies()
 
     const {value} = cookieStore.get('token') ?? {}
 
-    const headerParams = value ? {
-      ...header,
+    const headerParams = value ? Object.assign({}, header, {
       'Content-Type': 'application/json',
       'Authorization': value
-    } : {
-      ...header,
+    }) : Object.assign({}, header, {
       'Content-Type': 'application/json'
-    } 
+    });
     try {
       const response: any = await request({
           type: method,
@@ -29,21 +39,21 @@ const requestHandler = async (req: NextRequest, method: Method, url: string, bod
       return NextResponse.json({ ...response.data ?? [] }, { status: response.status });
 
     }catch (e) {
-        return NextResponse.json({ data: [] }, { status: e.status });
+        return NextResponse.json({ e }, { status: 500 });
     }
     
 };
 
 export async function GET(req: NextRequest, {params}: { params: { urls: string[] } }) {
   
-    const uri = params.urls.join('/');
-    const queryString = req.nextUrl.searchParams.toString();
-    return await requestHandler(req, 'GET', `${uri}?${queryString}`, {}, req.headers);
+  const uri = params.urls.join('/');
+  const queryString = req.nextUrl.searchParams.toString();
+  return await requestHandler(req, httpMethod.GET, `${uri}?${queryString}`, {}, req.headers);
 }
 
 export async function POST(req: NextRequest, {params}: { params: { urls: string[] } }) {
   const data = await req.json();
   const uri = params.urls.join('/');
   const queryString = req.nextUrl.searchParams.toString();
-  return await requestHandler(req, 'POST', `${uri}?${queryString}`, data, req.headers);
+  return await requestHandler(req, httpMethod.POST, `${uri}?${queryString}`, data, req.headers);
 }
