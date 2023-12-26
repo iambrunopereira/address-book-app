dc2d92eaa0e5de83fdf27d1db9b71b50eadc5454
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-26 18:21:58 +0000

    feat: added logo to the splash screen and change the app name style

--- src/components/structure/SplashScreen/component.tsx
+++ src/components/structure/SplashScreen/component.tsx
@@ -2,13 +2,15 @@
 
 import Box from '@/components/common/Box';
 import { LinearLoader } from '@/components/common/LinearLoader';
+import { Typography } from '@/components/common/Typography';
 import { config } from '@/configs';
+import Image from 'next/image';
 import type { FC } from 'react';
 import styles from "./component.module.scss";
-
 export const SplashScreen: FC = () => (
     <Box elementType="main" className={styles.splashScreen}>
-        <p>{config.appName}</p>
+        <Image src="/assets/logo.svg" width="100" height="100" alt="logo" />
+        <Typography variant='h2'>{config.appName}</Typography>
          <LinearLoader />
     </Box>
 );
